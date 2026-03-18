f8572f6def12cf1d276f29d2ae268e3554882d98
Mogiti-Joy <joylinemogiti@gmail.com>
2026-03-18 23:33:11 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -188,4 +188,4 @@ def collect_data():
                                 print("No articles collected")
                     except Exception as e:
                         print(f"Error saving data: {e}")
-collect_data()
+                    collect_data()
