8475853fbd5f15987296127da2c1a8ca7f4d081a
Mogiti-Joy <joylinemogiti@gmail.com>
2026-03-26 20:46:24 +0300

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -44,6 +44,8 @@ df['virality_score'] = (
 # ANALYTICS
 # 1. Source Influence
 source_volume = df.groupby('source').size()
+source_sentiment = df.groupby('source')['sentiment_counts'].mean()
+
 source_score = pd.DataFrame({
     "volume": source_volume,
     "sentiment": source_sentiment,
